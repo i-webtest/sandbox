c01a7fbe2348dff4dad2875fbe9b530189e1a635
i-webtest <stasbass80@gmail.com>
2022-02-28 16:10:32 +0300

    выполнен урок №3

--- js/script.js
+++ js/script.js
@@ -1,55 +1,43 @@
 "use strict";
 
-/* 
- Создайте массив arr с элементами 2, 5, 3, 9. Умножьте первый элемент массива на второй, а третий элемент  на четвертый. Результаты сложите, присвойте переменной result. Выведите на экран значение этой переменной.
-*/
-
-// let obj = {
-//   1: "понедельник",
-//   2: "вторник",
-//   3: "среда",
-//   4: "четверг",
-//   5: "пятница",
-//   6: "суббота",
-//   7: "воскресенье",
-// };
-
-// let day = obj[3];
-// console.log(day);
-
-// console.log(obj[6]);
-
-//Урок №1
-// let title, screens, screenPrice, rollback, fullPrice, adaptive;
-
-// alert("Hello, JavaScript!");
-// console.log("Hello, JavaScript!");
-
 //--------------------------------------------------------Урок №2
-const title = "Проект";
-const screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 500;
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+let screenPrice = +prompt("Сколько будет стоить данная работа?", 30000);
 let rollback = 10;
-let fullPrice = 30000;
-let adaptive = true;
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?", "слайдер");
+let servicePrice1 = +prompt("Сколько это будет стоить?", 2000);
+let service2 = prompt("Какой дополнительный тип услуги нужен?", "реклама");
+let servicePrice2 = +prompt("Сколько это будет стоить?", 1000);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let percentPrice = fullPrice * (rollback / 100);
+let servicePercentPrice = fullPrice - percentPrice;
+
+if (fullPrice >= 30000) {
+  console.log("Даём скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что то пошло не так");
+}
 
 console.log(typeof title);
+console.log("Проект: " + title);
 console.log(typeof fullPrice);
-console.log(typeof adaptive);
+
+console.log("Типы экранов: " + screens);
+console.log("Стоимость вёрстки экранов: " + screenPrice + " рублей");
+console.log(adaptive);
+console.log("Дополнительный тип услуги: " + service1);
+console.log("Стоимость дополнительной услуги: " + servicePrice1);
+console.log("Дополнительный тип услуги: " + service2);
+console.log("Стоимость дополнительной услуги: " + servicePrice2);
 console.log(screens.length);
 console.log(screens.toLowerCase().split(screens));
-console.log("Стоимость вёрстки экранов: " + screenPrice + " рублей");
+
 console.log("Стоимость разработки сайта: " + fullPrice + " рублей");
 console.log("Процент отката посреднику за работу: " + fullPrice * (rollback / 100));
-
-//УСЛОЖНЕННОЕ ЗАДАНИЕ
-let num = 266219;
-let num1 = Array.from(String(num), Number);
-let result = num1[0] * num1[1] * num1[2] * num1[3] * num1[4] * num1[5];
-result **= 3;
-
-console.log(num1);
-console.log(result);
-console.log(String(result).substring(0, 2));
-
-//---------------------------------------------------------Урок №3
+console.log("Итого: " + Math.ceil(servicePercentPrice));
